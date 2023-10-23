9fa6d598cd7a4ed598d691d74e6802ae93c33022
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-10-23 13:52:20 +0200

    Agregar archivos de proyecto.

---
+++ NumeroPar/controladores/Program.cs
@@ -0,0 +1,24 @@
+﻿using NumeroPar.servicios;
+
+namespace NumeroPar.controladores
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PeticionDeNumeroYMenuInterfaz objetoNumero = new PeticionDeNumeroYImplementacion();
+            int numeroseleccionado = objetoNumero.menuYNumero();
+            
+            if ((numeroseleccionado % 2)==0)
+            {
+                Console.WriteLine("El numero es par");
+            }else {
+                Console.WriteLine("El numero es impar");
+            }
+
+
+
+        }
+    }
+}
+
---
+++ NumeroPar/servicios/PeticionDeNumeroYMenuImplementacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPar.servicios
+{
+    internal class PeticionDeNumeroYImplementacion : PeticionDeNumeroYMenuInterfaz
+    {
+        public int menuYNumero()
+        {
+            Console.WriteLine("Escriba su numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            return numero;
+        }
+    }
+}
